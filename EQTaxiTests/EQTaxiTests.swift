30bc558b1f79cd6b11591d563c9fd663075a8679
Vijay Karthik Chandramouli <vijaykarthikmouli@gmail.com>
2018-01-23 14:58:22 +0530

    Initial Commit

---
+++ EQTaxiTests/EQTaxiTests.swift
@@ -0,0 +1,36 @@
+//
+//  EQTaxiTests.swift
+//  EQTaxiTests
+//
+//  Created by Equator Technologies on 23/01/18.
+//  Copyright © 2018 Equator Technologies. All rights reserved.
+//
+
+import XCTest
+@testable import EQTaxi
+
+class EQTaxiTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
