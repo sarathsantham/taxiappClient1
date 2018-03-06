054bddb7ec8e028b8320bb2f8b49960857b192ab
Equator Technologies <employeelogin@Equators-Mac-mini.local>
2018-03-06 12:32:59 +0530

    first commit

---
+++ EQTaxi/View/RideHistoryViewCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RideHistoryViewCellTableViewCell.swift
+//  EQTaxi
+//
+//  Created by Equator Technologies on 01/03/18.
+//  Copyright © 2018 Equator Technologies. All rights reserved.
+//
+
+import UIKit
+
+class RideHistoryViewCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
